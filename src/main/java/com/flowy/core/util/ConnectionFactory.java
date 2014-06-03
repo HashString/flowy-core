99d08d178a0b11e94a38792e114e369523d68fbf
shantanu <ssinghal@ptc.com>
2014-06-03 18:57:29 +0530

    #20 | create mongo connection using input from property files

--- src/main/java/com/flowy/core/util/ConnectionFactory.java
+++ src/main/java/com/flowy/core/util/ConnectionFactory.java
@@ -5,16 +5,29 @@ import com.mongodb.DBCollection;
 import com.mongodb.MongoClient;
 
 import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ConnectionFactory {
 
-    private MongoClient mongoClient;
+    private DB db;
+    private Map<String, DBCollection> collectionMap;
 
     public ConnectionFactory() throws UnknownHostException {
-        this.mongoClient = new MongoClient("localhost", 27017);
+        String hostName = ConfigHandler.get("mongo.local.host_name");
+        String dbName = ConfigHandler.get("mongo.local.database_name");
+        int port = Integer.parseInt(ConfigHandler.get("mongo.local.port"));
+
+        db = new MongoClient(hostName, port).getDB(dbName);
+        collectionMap = new HashMap<String, DBCollection>();
     }
 
-    public DBCollection getCollection(String dbName, String collectionName) throws UnknownHostException {
-        return mongoClient.getDB(dbName).getCollection(collectionName);
+    public DBCollection getCollection(String collectionName) throws UnknownHostException {
+        DBCollection dbCollection;
+        if((dbCollection = collectionMap.get(collectionName)) == null) {
+            collectionMap.put(collectionName, dbCollection = db.getCollection(collectionName));
+        }
+        return dbCollection;
     }
+
 }
